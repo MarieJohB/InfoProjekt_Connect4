b7c150247b1d729f3a0d9ce10304b04761fe25fb
Neyus282 <domidomi649@gmail.com>
2024-02-17 19:47:27 +0100

    Fix der zweiten Fehlereingabe der Zahl

--- src/Connect Four/Checker.cpp
+++ src/Connect Four/Checker.cpp
@@ -21,11 +21,14 @@ void Checker::checkTextAusgabe(char TextAusgeben) {
 
 void Checker::checkNumberAusgabe(int ausgabe) {
     // Prüfen, ob die Eingabe korrekt war
-    while (!(ausgabe == '1' || ausgabe == '2')) { // oder (ausgabe != '1' && ausgabe != '2')
+    while (!(ausgabe == 1 || ausgabe == 2)) { // oder (ausgabe != '1' && ausgabe != '2')
         system("cls"); // Für Windows
         cout << "Falsche Eingabe: " << endl << "1 => Spieler 1: X | Spieler 2: O \n2 => Spieler 1: O | Spieler 2: X \n" << endl;
         cout << "Ihre Eingabe: ";
         cin >> ausgabe;
+        if (ausgabe > 1000000000) {
+            ausgabe = 3;
+        }
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignoriert alle weiteren Zeichen
         system("cls"); // Für Windows
     }
