36b22da08cedd021646979830ff994910ad81123
MarieJohB <tristsnbecker624@gmail.com>
2024-03-01 11:33:59 +0100

    nur paar Kommentare

--- src/Connect Four/Konfiguration.h
+++ src/Connect Four/Konfiguration.h
@@ -9,7 +9,7 @@ class Konfiguration
 {
 public: 
 
-	char input;
+	char input; // Eingabe durch den User
 	bool displayText; //Bool um die Ausgabe zu wechseln
 	bool play; // Bool um in die Spielschleife zu gehen
 	bool end; // Bool um das Programm zu beenden
@@ -20,7 +20,8 @@ public:
 	Konfiguration();
 	
 
-	void askUser(string filename, Player& Spieler1, Player& Spieler2);
+	void askUser(string filename, Player& Spieler1, Player& Spieler2); 
+	// Uebergabe der Klasse mit CallByRefernece, wird weiter gegeben an startGame()
 
 	// Funktionen, die der User aufrufen kann
 	// Spiel starten:
