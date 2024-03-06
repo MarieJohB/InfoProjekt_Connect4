4a9952a27b7be79f2f0517c362c22f8bc7afe6ab
Neyus282 <domidomi649@gmail.com>
2024-03-06 11:41:40 +0100

    rgujgoijsjes

--- src/Connect Four/Ruler.cpp
+++ src/Connect Four/Ruler.cpp
@@ -4,7 +4,7 @@ using namespace::std;
 
 
 bool Ruler :: isValidMove(char board[ROWS][COLS], int col){
-    if (col == 251) { // Easter Egg wenn die Zahl 282 eingegeben wurde
+    if (col == 252) { // Easter Egg wenn die Zahl 251 eingegeben wurde
         Celebrater cel; 
         cel.victoryDance(); // Abspielen einer Animation im Terminal
     };
